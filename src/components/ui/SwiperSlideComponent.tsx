5095aad339c2e111b7814ea083b876d8766167c5
ahmadadejumo <adejumoahmad@gmail.com>
2023-11-16 12:50:09 +0100

    review feat and make changes

--- src/components/ui/SwiperSlideComponent.tsx
+++ src/components/ui/SwiperSlideComponent.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
-
-const TestimonialCard = ({content, name, bg, color, nameColor, qouteColor}) => {
+const TestimonialCard = ({
+  content,
+  name,
+  bg,
+  color,
+  nameColor,
+  qouteColor,
+}: {
+  content: string;
+  name: string;
+  bg?: string;
+  color?: string;
+  nameColor?: string;
+  qouteColor?: string;
+}) => {
   return (
-    <div className={`border-[1px] shadow-lg relative h-full border-solid border-white rounded-[0.9375rem] px-[4rem] md:px-[3rem] py-[3rem] md:py-[5rem]`} style={{backgroundColor: bg}}>
-     
-      <span style={{color: qouteColor}} className="absolute font-bold work-font text-[2.23931rem] md:text-[6rem] top-4 left-4">&#8220;</span>
+    <div
+      className={`border-[1px] shadow-lg relative h-full border-solid border-white rounded-[0.9375rem] px-[4rem] md:px-[3rem] py-[3rem] md:py-[5rem]`}
+      style={{ backgroundColor: bg }}
+    >
+      <span
+        style={{ color: qouteColor }}
+        className="absolute font-bold work-font text-[2.23931rem] md:text-[6rem] top-4 left-4"
+      >
+        &#8220;
+      </span>
 
-      <p className="mt-2 text-[0.34988rem] md:text-[0.9375rem] text-style work-font font-medium text-center leading-normal" style={{color: color}}>
-       {content}
+      <p
+        className="mt-2 text-[0.34988rem] md:text-[0.9375rem] text-style work-font font-medium text-center leading-normal"
+        style={{ color: color }}
+      >
+        {content}
       </p>
-      <span style={{color: qouteColor}} className="absolute font-bold work-font text-[2.23931rem] md:text-[6rem] right-10 md:right-4 md:top-[11rem]">&#8221;</span>
+      <span
+        style={{ color: qouteColor }}
+        className="absolute font-bold work-font text-[2.23931rem] md:text-[6rem] right-10 md:right-4 md:top-[11rem]"
+      >
+        &#8221;
+      </span>
 
       <div className="mt-4 flex justify-center">
-        <a href="#" className="text-[0.30325rem] md:text-[0.8125rem] work-font font-semibold leading-normal" tabIndex="0" role="link" style={{color: nameColor}}>
+        <a
+          href="#"
+          className="text-[0.30325rem] md:text-[0.8125rem] work-font font-semibold leading-normal"
+          tabIndex={0}
+          role="link"
+          style={{ color: nameColor }}
+        >
           {name}
         </a>
       </div>
--- src/components/ui/Testimonies.tsx
+++ src/components/ui/Testimonies.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
 import { Testimony } from "./Testimony";
 
 export const Testimonies = () => {
   return (
     <div className="bg-[#F3F5F6] border-2">
-        {/* <h1 className="text-[2.25rem] font-bold text-center tracking-[-0.125rem]">What Our Attendees <br/> Have To Say</h1> */}
-        <div>
+      {/* <h1 className="text-[2.25rem] font-bold text-center tracking-[-0.125rem]">What Our Attendees <br/> Have To Say</h1> */}
+      <div>
         <Testimony />
-
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
