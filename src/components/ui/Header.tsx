4ceffa5c93b8551c45ac05a5b4fba0b8de001b28
alajido <96017433+Alajido@users.noreply.github.com>
2023-11-16 10:30:39 +0100

    working on the slider

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,51 +1,24 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 import { Button } from "./button";
-// import { Link } from "react-router-dom";
-// import { X } from "lucide-react";
 import arrow from "../../assets/arrow.svg";
-// import menu from "../../assets/menu.svg";
 import logo from "../../assets/logo.png";
 import smalllogo from "../../assets/smalllogo.png";
 import { motion } from "framer-motion";
 
 
-interface HeaderProps {}
-
 export const Header: React.FC<HeaderProps> = () => {
-  const [active, setIsActive] = useState<boolean>(false);
-  const menuRef = useRef<HTMLUListElement | null>(null);
-
-  const handleNavBar = () => {
-    setIsActive(!active);
-    console.log(active);
-  };
-
 
   return (
     <nav className="bg-transparent w-full">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <div className="flex items-center gap-2 justify-between">
-        <img className='w-[2.25rem] h-[1.09256rem] md:hidden' src={smalllogo} alt="" srcset="" />
+        <img className='w-[2.25rem] h-[1.09256rem] md:hidden' src={smalllogo} alt="" />
 
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
             id="navbar-sticky"
           >
-            <img src={logo} className="w-[5.79194rem] h-[2.8125rem] hidden md:block" alt="" srcset="" />
-            {/* <ul className="flex flex-col text-[#1E1E1E] md:flex-row md:gap-[1.9375rem] ">
-              <li className="text-[1.1rem] font-semibold text-style tracking-[-0.025rem]">
-                <Link to="/"> Home</Link>
-              </li>
-              <li className="text-[1.1rem] font-semibold text-style tracking-[-0.025rem]">
-                <Link to="/agenda"> Agenda</Link>
-              </li>
-              <li className="text-[1.1rem] font-semibold text-style tracking-[-0.025rem]">
-                <Link to="/contact"> Contact</Link>
-              </li>
-              <li className="text-[1.1rem] font-semibold text-style tracking-[-0.025rem]">
-                <Link to="/sponsors"> Sponsors</Link>
-              </li>
-            </ul> */}
+            <img src={logo} className="w-[4rem] h-[2rem] hidden md:block" alt="" />
           </div>
         </div>
 
@@ -62,46 +35,6 @@ export const Header: React.FC<HeaderProps> = () => {
           </Button>
         </motion.div>
 
-        {/* <ul
-          ref={menuRef}
-          className={`mobile-menu ${
-            active ? "w-[100%] bg-white dark:bg-[#020617]" : "w-0"
-          } h-screen overflow-hidden transition-all duration-300 ease-in-out z-50 absolute top-0 left-0 lg:hidden`}
-        >
-          <ul className="mt-10 ml-3">
-            <li className="mb-4 text-[1.2rem] flex justify-between items-center font-semibold text-style tracking-[-0.025rem]">
-              <Link to="/" onClick={() => setIsActive(false)}>
-                Home
-              </Link>
-              {active && (
-                <X
-                  onClick={handleNavBar}
-                  className="close border-primary border-[1px] w-[1.5rem] h-[1.5rem] right-4 cursor-pointer font-medium text-[40px] leading-5 not-italic absolute"
-                />
-              )}
-            </li>
-            <li className="mb-4 text-[1.2rem] font-semibold text-style tracking-[-0.025rem]">
-              <Link to="/agenda" onClick={() => setIsActive(false)}>
-                Agenda
-              </Link>
-            </li>
-            <li className="mb-4 text-[1.2rem] font-semibold text-style tracking-[-0.025rem]">
-              <Link to="/" onClick={() => setIsActive(false)}>
-                Speakers
-              </Link>
-            </li>
-            <li className="mb-4 text-[1.2rem] font-semibold text-style tracking-[-0.025rem]">
-              <Link to="/sponsors" onClick={() => setIsActive(false)}>
-                Sponsors
-              </Link>
-            </li>
-            <li className="mb-4 text-[1.2rem] font-semibold text-style tracking-[-0.025rem]">
-              <Link to="/contact" onClick={() => setIsActive(false)}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </ul> */}
       </div>
     </nav>
   );
--- src/components/ui/Testimony.tsx
+++ src/components/ui/Testimony.tsx
@@ -16,22 +16,20 @@ import SwiperSlideComponent from "./SwiperSlideComponent"
 export const Testimony = () => {
     const swiper = useSwiper()
   return (
-    <div className="border-2 border-red-500 mt-10 bg-[#F3F5F6]">
+    <div className="mt-10 bg-[#F3F5F6] rel">
 
         <h2 className="text-center text-[1.3rem] text-[#1E1E1E] text-style md:text-[2.5rem] font-bold leading-normal tracking-[-0.0625rem]">What Our Attendees <br/> Have To Say</h2>
 
         <div className="flex pb-20">
 
-
-         <div className="border-2 border-black w-[30%] flex justify-center items-center">
+         <div className="border-black hidden w-[30%] md:flex justify-center items-center">
            <button onClick={() => swiper.slideNext()}>
-<img src={next} alt="arrow" srcset="" className="w-[4.10544rem] md:w-[11rem]" />
+          <img src={next} alt="arrow"  className="w-[4.10544rem] md:w-[11rem]" />
            </button>
-            
         </div>
 
         <Swiper
-         className="border-2 border-green-500 w-[80%] "
+         className="w-[80%] mt-6"
         modules={[Navigation, A11y, Autoplay]}
         autoplay={{
           delay: 30000,
@@ -40,7 +38,6 @@ export const Testimony = () => {
         // slidesPerView={3}
         navigation
         pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
         breakpoints={{
           // >=200
           200: {
@@ -54,16 +51,16 @@ export const Testimony = () => {
           },
           // >= 640px
           640: {
-            slidesPerView: 1.7,
+            slidesPerView: 1.8,
             // spaceBetween: 5,
           },
           // >= 1024px
           1024: {
-            slidesPerView: 1.7,
+            slidesPerView: 1.8,
             spaceBetween: 10,
           },
           1030: {
-            slidesPerView: 1.7,
+            slidesPerView: 1.8,
             spaceBetween: 25,
           },
           // >= 1583px
@@ -73,13 +70,21 @@ export const Testimony = () => {
           },
         }}>
       <SwiperSlide className="">
-        <SwiperSlideComponent nameColor="#1E1E1E" color="#089D57"  bg='White' name="Sadiya, Bayero University Kano" content="As a student, I’m constantly seeking opportunities to engage with technology in a meaningful way. DevFest Student Edition Kano is the perfect platform for me to do just that. I’m excited to participate in work-
+        <SwiperSlideComponent qouteColor="#4285F4" nameColor="#1E1E1E" color="#089D57"  bg='White' name="Sadiya, Bayero University Kano" content="As a student, I’m constantly seeking opportunities to engage with technology in a meaningful way. DevFest Student Edition Kano is the perfect platform for me to do just that. I’m excited to participate in work-
             shops, hackathons and other interactive sessions that will challenge my creativity."/>
       </SwiperSlide>
       <SwiperSlide className="">
-        <SwiperSlideComponent nameColor="white" color="white" bg="#34A853" name="Ayomide, ADUSTECH" content="I’m eager to participate in DevFest Student Edition Kano and collaborate with students from BUK and NorthWest University. I believe that by wor-king together, we can share ideas and develop innovative solutions to real world problems."/>
+        <SwiperSlideComponent qouteColor="#fff" nameColor="white" color="white" bg="#34A853" name="Ayomide, ADUSTECH" content="I’m eager to participate in DevFest Student Edition Kano and collaborate with students from BUK and NorthWest University. I believe that by wor-king together, we can share ideas and develop innovative solutions to real world problems."/>
       </SwiperSlide>
-     
+      <SwiperSlide className="">
+        <SwiperSlideComponent 
+        nameColor="#1E1E1E" qouteColor="#4285F4" color="#089D57"  bg='White' name="Sadiya, Bayero University Kano" content='As a student, I’m constantly seeking opportunities to engage with technology in a meaningful way. DevFest Student Edition Kano is the perfect platform for me to do just that. I’m excited to participate in work-
+            shops, hackathons and other interactive sessions that will challenge my creativity.'/>
+      </SwiperSlide>
+
+        <SwiperSlide className="">
+        <SwiperSlideComponent qouteColor="#fff" nameColor="white" color="white" bg="#34A853" name="Ayomide, ADUSTECH" content="I’m eager to participate in DevFest Student Edition Kano and collaborate with students from BUK and NorthWest University. I believe that by wor-king together, we can share ideas and develop innovative solutions to real world problems."/>
+      </SwiperSlide>
       
         </Swiper>
         </div>
